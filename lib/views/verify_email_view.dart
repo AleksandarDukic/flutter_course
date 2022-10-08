637439500f0e05af76aa84e96ba2793dda5763c9
unknown <aleksandar.z.dukic@gmail.com>
2022-10-08 17:36:21 +0200

    Added Verify Email View into action

--- lib/views/verify_email_view.dart
+++ lib/views/verify_email_view.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_course/constants/routes.dart';
 
 class VerifyEmailView extends StatefulWidget {
   const VerifyEmailView({Key? key}) : super(key: key);
@@ -12,15 +13,34 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Verify email')),
+      appBar: AppBar(
+        title: const Text('Verify email'),
+      ),
       body: Column(children: [
-        const Text('Please verify your email address'),
+        const Text(
+          "We've sent you an email verification. Please open it to verify your account.",
+        ),
+        const Text(
+          "If you haven't recieved verification email, press the button below.",
+        ),
         TextButton(
-            onPressed: () async {
-              final user = FirebaseAuth.instance.currentUser;
-              await user?.sendEmailVerification();
-            },
-            child: const Text('Send email verification'))
+          onPressed: () async {
+            final user = FirebaseAuth.instance.currentUser;
+            await user?.sendEmailVerification();
+          },
+          child: const Text('Send email verification'),
+        ),
+        TextButton(
+          onPressed: () async {
+            await FirebaseAuth.instance.signOut();
+            if (!mounted) return;
+            Navigator.of(context).pushNamedAndRemoveUntil(
+              registerRoute,
+              (route) => false,
+            );
+          },
+          child: const Text('Restart'),
+        )
       ]),
     );
   }
